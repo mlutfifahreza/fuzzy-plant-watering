b23c2ae39063417d79c82a5e9e85daa5474b5e84
Muhammad Fahreza <mlutfifahreza@gmail.com>
2020-12-07 16:20:06 +0700

    update rules

--- FuzzySystem.py
+++ FuzzySystem.py
@@ -1,9 +1,7 @@
 from LinguisticVar.Moisture import Moisture
-from LinguisticVar.Intensity import Intensity
-from LinguisticVar.Leaves import Leaves
-from LinguisticVar.Water import Water
-from LinguisticVar.Height import Height
 from LinguisticVar.Temperature import Temperature
+from LinguisticVar.Height import Height
+from LinguisticVar.Water import Water
 
 class Rule:
     def __init__(self, lingustics=(), terms=()):
@@ -21,18 +19,14 @@ class Rule:
             value = min(value, self.M.getMoistValue(m))
         elif term == "wet":
             value = min(value, self.M.getWetValue(m))
-        # # count and update value for intensity
-        # term = self.intensity
-        # if term == "dark":
-        #     value = min(value, self.I.getDarkValue(i))
-        # elif term == "bright":
-        #     value = min(value, self.I.getBrightValue(i))
-        # # count and update value for leaves
-        # term = self.leaves
-        # if term == "few":
-        #     value = min(value, self.L.getFewValue(l))
-        # elif term == "many":
-        #     value = min(value, self.L.getManyValue(l))
+        # count and update value for temperature
+        term = self.temperature
+        if term == "cold":
+            value = min(value, self.T.getColdValue(t))
+        elif term == "normal":
+            value = min(value, self.T.getNormalValue(t))
+        elif term == "hot":
+            value = min(value, self.T.getHotValue(t))
         # count and update value for height
         term = self.height
         if term == "small":
@@ -41,13 +35,6 @@ class Rule:
             value = min(value, self.H.getMediumValue(h))
         elif term == "big":
             value = min(value, self.H.getBigValue(h))
-        term = self.temperature
-        if term == "cold":
-            value = min(value, self.T.getColdValue(t))
-        elif term == "normal":
-            value = min(value, self.T.getNormalValue(t))
-        elif term == "hot":
-            value = min(value, self.T.getHotValue(t))
         # find the value of watering
         volume = 0
         term = self.water
@@ -62,7 +49,8 @@ class Rule:
         elif term == "rather lots":
             volume = self.W.getRatherLotsDomain(value)
         elif term == "very lots":
-            volume = self.W.geVerytLotsDomain(value)
+            volume = self.W.getVeryLotsDomain(value)
+        # function output
         return value, volume
 
 
@@ -72,61 +60,57 @@ LinguisticVar = (M,T,H,W)
 
 # ADDING RULES
 terms = [
-    ("dry","cold","small","little"),
-    # ("dry","cold","small","lots"),
-    ("dry","cold","medium","little"),
-    ("dry","cold","medium","lots"),
-    ("dry","cold","big","little"),
-    ("dry","cold","big","lots"),
-    ("dry","normal","small","little"),
-    # ("dry","normal","small","lots"),
-    ("dry","normal","medium","little"),
-    ("dry","normal","medium","lots"),
-    # ("dry","normal","big","little"),
-    ("dry","normal","big","lots"),
-    ("dry","hot","small","little"),
-    ("dry","hot","small","lots"),
-    # ("dry","hot","medium","little"),
-    ("dry","hot","medium","lots"),
-    # ("dry","hot","big","little"),
-    ("dry","hot","big","lots"),
-    ("moist","cold","small","little"),
-    # ("moist","cold","small","lots"),
-    ("moist","cold","medium","little"),
-    # ("moist","cold","medium","lots"),
-    ("moist","cold","big","little"),
-    ("moist","cold","big","lots"),
-    ("moist","normal","small","little"),
-    # ("moist","normal","small","lots"),
-    ("moist","normal","medium","little"),
-    ("moist","normal","medium","lots"),
-    ("moist","normal","big","little"),
-    ("moist","normal","big","lots"),
-    ("moist","hot","small","little"),
-    ("moist","hot","small","lots"),
-    ("moist","hot","medium","little"),
-    ("moist","hot","medium","lots"),
-    ("moist","hot","big","little"),
-    ("moist","hot","big","lots"),
-    ("wet","cold","small","little"),
-    # ("wet","cold","small","lots"),
-    ("wet","cold","medium","little"),
-    # ("wet","cold","medium","lots"),
-    ("wet","cold","big","little"),
-    ("wet","cold","big","lots"),
-    ("wet","normal","small","little"),
-    # ("wet","normal","small","lots"),
-    ("wet","normal","medium","little"),
-    ("wet","normal","medium","lots"),
-    ("wet","normal","big","little"),
-    ("wet","normal","big","lots"),
+    # LITTLE PLANT
+    # very little
+    ("wet","cold","small","very little"),
+    ("wet","normal","small","very little"),
+    # little
     ("wet","hot","small","little"),
-    # ("wet","hot","small","lots"),
-    ("wet","hot","medium","little"),
-    ("wet","hot","medium","lots"),
-    ("wet","hot","big","little"),
+    ("moist","cold","small","little"),
+    ("moist","normal","small","little"),
+    # rather little
+    ("moist","hot","small","rather little"),
+    ("dry","cold","small","rather little"),
+    # rather lots
+    ("dry","normal","small","rather lots"),
+    ("dry","hot","small","rather lots"),
+    # lots
+    # very lots
+    
+    # MEDIUM PLANT
+    # very little
+    # little
+    ("wet","cold","medium","little"),
+    ("wet","normal","medium","little"),
+    # rather little
+    ("wet","hot","medium","rather little"),
+    ("moist","cold","medium","rather little"),
+    # rather lots
+    ("moist","normal","medium","rather lots"),
+    ("moist","hot","medium","rather lots"),
+    # lots
+    ("dry","cold","medium","lots"),
+    ("dry","normal","medium","lots"),
+    # very lots
+    ("dry","hot","medium","very lots"),
+    
+    # BIG PLANT
+    # very little
+    # little
+    # rather little
+    # rather lots
+    ("wet","cold","big","rather lots"),
+    ("wet","normal","big","rather lots"),
+    # lots
     ("wet","hot","big","lots"),
-    ]
+    ("moist","cold","big","lots"),
+    ("moist","normal","big","lots"),
+    ("moist","hot","big","lots"),
+    ("dry","cold","big","lots"),
+    # very lots
+    ("dry","normal","big","very lots"),
+    ("dry","hot","big","very lots"),
+]
 rules = []
 for term in terms:
     rules.append(Rule(LinguisticVar, term))
@@ -137,9 +121,9 @@ moistureValue = 6
 temperatureValue = 50
 heightValue = 40
 # Input from user (sensor)
-moistureValue = int(input("moisture : "))
-temperatureValue = int(input("temperature : "))
-heightValue = int(input("height : "))
+# moistureValue = int(input("moisture : "))
+# temperatureValue = int(input("temperature : "))
+# heightValue = int(input("height : "))
 
 # PROCESSING
 vars = (moistureValue, temperatureValue, heightValue)
